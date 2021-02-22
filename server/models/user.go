8709d4fbae55e7f85b724013355582adf882d1ad
chenlifeng <chenlifeng@wezhuiyi.com>
2021-02-22 15:37:19 +0800

    pidata的历史记录到mongo

--- server/models/user.go
+++ server/models/user.go
@@ -13,7 +13,7 @@ type User struct {
 	Pis []Pi `gorm:"foreignKey:UserId"`
 }
 type UserDto struct {
-	ID uint`json:"ID"`
+	ID     uint   `json:"ID"`
 	Name   string `json:"Name"`
 	Phone  string `json:"Phone"`
 	Email  string `json:"Email"`
@@ -22,7 +22,7 @@ type UserDto struct {
 
 func ToUserDto(user User) UserDto {
 	return UserDto{
-		ID: user.ID,
+		ID:     user.ID,
 		Name:   user.Name,
 		Phone:  user.Phone,
 		Email:  user.Email,
