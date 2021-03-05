14e67015e932a5d9b08afec907523c8d44e32124
chenlifeng <chenlifeng@wezhuiyi.com>
2021-03-05 16:50:47 +0800

    redis 单点登录

--- server/router/device.go
+++ server/router/device.go
@@ -8,18 +8,16 @@ import (
 
 func DeviceRouter(r *gin.Engine) *gin.Engine {
 
-
-
 	pi := r.Group("api/device/pi").Use(middleware.TokenMiddleware())
 	pi.POST("/register", controllers.RegisterPi)
 	pi.POST("/set/property", controllers.UserSetPiProperty)
 	pi.GET("/get/property", controllers.UserGetPiProperty)
 	pi.POST("/bind/user", controllers.UserBindPi)
-	pi.POST("user/list",controllers.UserPiList)
+	pi.POST("user/list", controllers.UserPiList)
 	pi.POST("list", controllers.PiList)
 	pi.POST("delete", controllers.PiDelete)
-	pi.POST("user/login/out",controllers.PiUserLoginOut)
-
-	r.POST("api/device/pi/login/out",controllers.PiLoginOut)
+	pi.POST("user/login/out", controllers.PiUserLoginOut)
+	pi.GET("get/history", controllers.UserGetPiHistoryData)
+	r.POST("api/device/pi/login/out", controllers.PiLoginOut)
 	return r
 }
